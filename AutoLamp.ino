6d7e9a944b326d67719c3d9a6dca0069f0182041
hshshv <53831820+hshshv@users.noreply.github.com>
2020-04-01 09:38:40 +0300

    Update AutoLamp.ino

--- AutoLamp.ino
+++ AutoLamp.ino
@@ -1,8 +1,3 @@
-/*
-   מנורה אוטומטית
-   עם חיישן אולטראסוני \ נפח
-*/
-
 #include "Sensors.h" //download from: github.com/hshshv/Sensors
 #include "Led.h" //download from:  github.com/hshshv/Led
 
@@ -13,20 +8,19 @@
 #define poten A1
 #define TurnedOnTime 10000
 
-UltrasonicSensor sens(trig, acho);
-Led lamp(ledpin);
+UltrasonicSensor sens(trig, acho); //creat an object of 'ultrasonicSensor' class namd 'sens'
+Led lamp(ledpin); //creat an object of 'Led' class named 'lamp'
 
 void setup()
 {
-  sens.StoppingDistance = 40;
-  digitalWrite(13, LOW);
+  sens.StoppingDistance = 40;//the sensor will be considered 'activated' if the distance it gets is less then 40 sm.
 }
 
 unsigned int StopAt;
 
 void loop()
 {
-  if (sens.Activated() && analogRead(LDR) < analogRead(poten))
+  if (sens.Activated() && analogRead(LDR) < analogRead(poten)) //if the voltage from the LDR is lower then the voltage from the potentiometer it means thet ther is not enought light in the room
   {
     lamp.TurnOn();
     StopAt = 0;
